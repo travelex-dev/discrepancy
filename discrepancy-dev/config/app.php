155eecf53634f7aa18ce0798be68d50ea4718aa3
Azhar Ahmad <azharahmad1@live.co.uk>
2016-06-02 15:30:38 +0100

    test
    test

--- discrepancy-dev/config/app.php
+++ discrepancy-dev/config/app.php
@@ -90,9 +90,10 @@ return [
     | will not be safe. Please do this before deploying an application!
     |
     */
-    'key' => env('APP_KEY', 'SomeRandomString'),
 
-    'cipher' => MCRYPT_RIJNDAEL_128,
+    'key' => env('APP_KEY'),
+
+    'cipher' => 'AES-256-CBC',
 
     /*
     |--------------------------------------------------------------------------
