9ff86847446d86a0b7e0083bcffbbba483671f89
AnijGurung <aniz_grg@hotmail.com>
2016-06-09 09:37:34 +0100

    Dashboard page
    1) Removed menu duplicates from investigating errors
    2) Added back the removed dashboard page

---
+++ discrepancy-dev/resources/views/pages/dashboard.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.master')
+
+@section('content')
+		<!-- BEGIN BASE-->
+		<div id="base">
+
+			<!-- BEGIN OFFCANVAS LEFT -->
+			<div class="offcanvas">
+			</div><!--end .offcanvas-->
+			<!-- END OFFCANVAS LEFT -->
+
+			<!-- BEGIN CONTENT-->
+			<div id="content">
+
+				<section>
+					<div class="section-body">
+						<div class="row">
+
+							<!-- BEGIN ALERT - REVENUE -->
+							<div class="col-md-3 col-sm-6">
+								<div class="card">
+									<div class="card-body no-padding">
+										<div class="alert alert-callout alert-info no-margin">
+											<strong class="pull-right text-success text-lg">0,38% <i class="md md-trending-up"></i></strong>
+											<strong class="text-xl">34</strong><br>
+											<span class="opacity-50">Errors Outstanding</span>
+										</div>
+									</div><!--end .card-body -->
+								</div><!--end .card -->
+							</div><!--end .col -->
+							<!-- END ALERT - REVENUE -->
+
+							<!-- BEGIN ALERT - VISITS -->
+							<div class="col-md-3 col-sm-6">
+								<div class="card">
+									<div class="card-body no-padding">
+										<div class="alert alert-callout alert-warning no-margin">
+											<strong class="pull-right text-warning text-lg">0,01% <i class="md md-swap-vert"></i></strong>
+											<strong class="text-xl">$2,366.12</strong><br>
+											<span class="opacity-50">GBP Outstanding</span>
+										</div>
+									</div><!--end .card-body -->
+								</div><!--end .card -->
+							</div><!--end .col -->
+							<!-- END ALERT - VISITS -->
+
+							<!-- BEGIN ALERT - BOUNCE RATES -->
+							<div class="col-md-3 col-sm-6">
+								<div class="card">
+									<div class="card-body no-padding">
+										<div class="alert alert-callout alert-danger no-margin">
+											<strong class="pull-right text-danger text-lg">0,18% <i class="md md-trending-down"></i></strong>
+											<strong class="text-xl">46.90%</strong><br>
+											<span class="opacity-50">Matched rate</span>
+										</div>
+									</div><!--end .card-body -->
+								</div><!--end .card -->
+							</div><!--end .col -->
+							<!-- END ALERT - BOUNCE RATES -->
+
+							<!-- BEGIN ALERT - TIME ON SITE -->
+							<div class="col-md-3 col-sm-6">
+								<div class="card">
+									<div class="card-body no-padding">
+										<div class="alert alert-callout alert-success no-margin">
+											<h1 class="pull-right text-success"><i class="md md-timer"></i></h1>
+											<strong class="text-xl">2,5 days.</strong><br>
+											<span class="opacity-50">Avg. time to resolve error</span>
+										</div>
+									</div><!--end .card-body -->
+								</div><!--end .card -->
+							</div><!--end .col -->
+						</div><!--end .row -->
+					</div><!--end .section-body -->
+				</section>
+
+
+			</div>
+		</div><!--end #base-->
+		<!-- END BASE -->
+
+@stop
