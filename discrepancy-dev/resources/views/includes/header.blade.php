8dd14245ce44fb7ea3898bb20f75ac6106f752f0
Baldarius <oliverb@laptop-oby.gcs-taxidia.net>
2016-06-09 09:56:00 +0100

    Fixed Logout causing menu on login
    Changed the link to make sure that it doesn’t cause the menu to load up
    on the login page when clicking “Logout”

--- discrepancy-dev/resources/views/includes/header.blade.php
+++ discrepancy-dev/resources/views/includes/header.blade.php
@@ -6,7 +6,7 @@
 					<ul class="header-nav header-nav-options">
 						<li class="header-nav-brand" >
 							<div class="brand-holder">
-								<a href="../../html/dashboards/dashboard.html">
+								<a href="dashboard">
 									<span class="text-lg text-bold text-primary">TAXIDIA</span>
 								</a>
 							</div>
@@ -25,21 +25,23 @@
 					<ul class="header-nav header-nav-profile">
 						<li class="dropdown">
 							<a href="javascript:void(0);" class="dropdown-toggle ink-reaction" data-toggle="dropdown">
-								<img src="../../assets/img/avatar1.jpg?1403934956" alt="" />
+								<img src="../../assets/img/avatar1.jpg?1403934956" alt="">
 								<span class="profile-info">
 									Sebastiaan van Wijngaarden
 									<small>Technology Manager</small>
 								</span>
 							</a>
 							<ul class="dropdown-menu animation-dock">
-								<li class="dropdown-header">Config</li>
-								<li><a href="../../html/pages/profile.html">My profile</a></li>
-								<li><a href="../../html/pages/blog/post.html">My blog <span class="badge style-danger pull-right">16</span></a></li>
-								<li><a href="../../html/pages/calendar.html">My appointments</a></li>
-								<li class="divider"></li>
-								<li><a href="../../html/pages/locked.html"><i class="fa fa-fw fa-lock"></i> Lock</a></li>
-								<li><a href="../../html/pages/login.html"><i class="fa fa-fw fa-power-off text-danger"></i> Logout</a></li>
-							</ul><!--end .dropdown-menu -->
+							<li class="dropdown-header">Config</li>
+							<li><a href="../../html/pages/profile.html">My profile</a></li>
+							<li><a href="../../html/pages/blog/post.html">My blog <span class="badge style-danger pull-right">16</span></a></li>
+							<li><a href="../../html/pages/calendar.html">My appointments</a></li>
+							<li><a href="bugreport">Bug Report</a></li>
+							<li><a href="feedback">Feedback</a></li>
+							<li class="divider"></li>
+							<li><a href="../../html/pages/locked.html"><i class="fa fa-fw fa-lock"></i> Lock</a></li>
+							<li><a href="/"><i class="fa fa-fw fa-power-off text-danger"></i> Logout</a></li>
+						</ul><!--end .dropdown-menu -->
 						</li><!--end .dropdown -->
 					</ul><!--end .header-nav-profile -->
 					<ul class="header-nav header-nav-toggle">
