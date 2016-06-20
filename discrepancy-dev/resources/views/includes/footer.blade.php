5e48a8e5c10aaae03e3f898f0bc27aa9a623e16d
AnijGurung <aniz_grg@hotmail.com>
2016-06-20 10:19:24 +0100

    Updated footer.blade.php with proper tags
    Updated the code script= to Html::script.

--- discrepancy-dev/resources/views/includes/footer.blade.php
+++ discrepancy-dev/resources/views/includes/footer.blade.php
@@ -1,54 +1,54 @@
 
 		<!-- BEGIN JAVASCRIPT -->
-		<script src="assets/js/libs/jquery/jquery-1.11.2.js"></script>
-		<script src="assets/js/libs/jquery/jquery-1.11.2.min.js"></script>
-		<script src="assets/js/libs/jquery/jquery-migrate-1.2.1.min.js"></script>
-		<script src="assets/js/libs/bootstrap/bootstrap.min.js"></script>
-		<script src="assets/js/libs/spin.js/spin.min.js"></script>
-		<script src="assets/js/libs/autosize/jquery.autosize.min.js"></script>
-		<script src="assets/js/libs/moment/moment.min.js"></script>
-		<script src="assets/js/libs/flot/jquery.flot.min.js"></script>
-		<script src="assets/js/libs/flot/jquery.flot.time.min.js"></script>
-		<script src="assets/js/libs/flot/jquery.flot.resize.min.js"></script>
-		<script src="assets/js/libs/flot/jquery.flot.orderBars.js"></script>
-		<script src="assets/js/libs/flot/jquery.flot.pie.js"></script>
-		<script src="assets/js/libs/flot/curvedLines.js"></script>
-		<script src="assets/js/libs/jquery-knob/jquery.knob.min.js"></script>
-		<script src="assets/js/libs/sparkline/jquery.sparkline.min.js"></script>
-		<script src="assets/js/libs/nanoscroller/jquery.nanoscroller.min.js"></script>
-		<script src="assets/js/libs/d3/d3.min.js"></script>
-		<script src="assets/js/libs/d3/d3.v3.js"></script>
-		<script src="assets/js/libs/rickshaw/rickshaw.min.js"></script>
-		<script src="assets/js/core/source/App.js"></script>
-		<script src="assets/js/core/source/AppNavigation.js"></script>
-		<script src="assets/js/core/source/AppOffcanvas.js"></script>
-		<script src="assets/js/core/source/AppCard.js"></script>
-		<script src="assets/js/core/source/AppForm.js"></script>
-		<script src="assets/js/core/source/AppNavSearch.js"></script>
-		<script src="assets/js/core/source/AppVendor.js"></script>
-		<script src="assets/js/core/demo/Demo.js"></script>
-		<script src="assets/js/core/demo/DemoDashboard.js"></script>
-		<script src="assets/js/libs/DataTables/jquery.dataTables.min.js"></script>
-		<script src="assets/js/libs/DataTables/extensions/ColVis/js/dataTables.colVis.min.js"></script>
-		<script src="assets/js/libs/DataTables/extensions/TableTools/js/dataTables.tableTools.min.js"></script>
-		<script src="assets/js/libs/jquery-validation/dist/jquery.validate.min.js"></script>
-		<script src="assets/js/libs/jquery-validation/dist/additional-methods.min.js"></script>
-		<script src="assets/js/libs/wizard/jquery.bootstrap.wizard.min.js"></script>
-		<script src="assets/js/core/demo/DemoFormWizard.js"></script>
-		<script src="assets/js/libs/jquery-ui/jquery-ui.min.js"></script>
-		<script src="assets/js/libs/select2/select2.min.js"></script>
-		<script src="assets/js/libs/bootstrap-tagsinput/bootstrap-tagsinput.min.js"></script>
-		<script src="assets/js/libs/multi-select/jquery.multi-select.js"></script>
-		<script src="assets/js/libs/inputmask/jquery.inputmask.bundle.min.js"></script>
-		<script src="assets/js/libs/moment/moment.min.js"></script>
-		<script src="assets/js/libs/bootstrap-datepicker/bootstrap-datepicker.js"></script>
-		<script src="assets/js/libs/bootstrap-colorpicker/bootstrap-colorpicker.min.js"></script>
-		<script src="assets/js/core/demo/DemoFormComponents.js"></script>
-		<script src="assets/js/core/demo/DemoTableDynamic.js"></script>
+		{!! Html::script('assets/js/libs/jquery/jquery-1.11.2.js') !!}
+		{!! Html::script('assets/js/libs/jquery/jquery-1.11.2.min.js') !!}
+		{!! Html::script('assets/js/libs/jquery/jquery-migrate-1.2.1.min.js') !!}
+		{!! Html::script('assets/js/libs/bootstrap/bootstrap.min.js') !!}
+		{!! Html::script('assets/js/libs/spin.js/spin.min.js') !!}
+		{!! Html::script('assets/js/libs/autosize/jquery.autosize.min.js') !!}
+		{!! Html::script('assets/js/libs/moment/moment.min.js') !!}
+		{!! Html::script('assets/js/libs/flot/jquery.flot.min.js') !!}
+		{!! Html::script('assets/js/libs/flot/jquery.flot.time.min.js') !!}
+		{!! Html::script('assets/js/libs/flot/jquery.flot.resize.min.js') !!}
+		{!! Html::script('assets/js/libs/flot/jquery.flot.orderBars.js') !!}
+		{!! Html::script('assets/js/libs/flot/jquery.flot.pie.js') !!}
+		{!! Html::script('assets/js/libs/flot/curvedLines.js') !!}
+		{!! Html::script('assets/js/libs/jquery-knob/jquery.knob.min.js') !!}
+		{!! Html::script('assets/js/libs/sparkline/jquery.sparkline.min.js') !!}
+		{!! Html::script('assets/js/libs/nanoscroller/jquery.nanoscroller.min.js') !!}
+		{!! Html::script('assets/js/libs/d3/d3.min.js') !!}
+		{!! Html::script('assets/js/libs/d3/d3.v3.js') !!}
+		{!! Html::script('assets/js/libs/rickshaw/rickshaw.min.js') !!}
+		{!! Html::script('assets/js/core/source/App.js') !!}
+		{!! Html::script('assets/js/core/source/AppNavigation.js') !!}
+		{!! Html::script('assets/js/core/source/AppOffcanvas.js') !!}
+		{!! Html::script('assets/js/core/source/AppCard.js') !!}
+		{!! Html::script('assets/js/core/source/AppForm.js') !!}
+		{!! Html::script('assets/js/core/source/AppNavSearch.js') !!}
+		{!! Html::script('assets/js/core/source/AppVendor.js') !!}
+		{!! Html::script('assets/js/core/demo/Demo.js') !!}
+		{!! Html::script('assets/js/core/demo/DemoDashboard.js') !!}
+		{!! Html::script('assets/js/libs/DataTables/jquery.dataTables.min.js') !!}
+		{!! Html::script('assets/js/libs/DataTables/extensions/ColVis/js/dataTables.colVis.min.js') !!}
+		{!! Html::script('assets/js/libs/DataTables/extensions/TableTools/js/dataTables.tableTools.min.js') !!}
+		{!! Html::script('assets/js/libs/jquery-validation/dist/jquery.validate.min.js') !!}
+		{!! Html::script('assets/js/libs/jquery-validation/dist/additional-methods.min.js') !!}
+		{!! Html::script('assets/js/libs/wizard/jquery.bootstrap.wizard.min.js') !!}		
+		{!! Html::script('assets/js/core/demo/DemoFormWizard.js') !!}		
+		{!! Html::script('assets/js/libs/jquery-ui/jquery-ui.min.js') !!}		
+		{!! Html::script('assets/js/libs/select2/select2.min.js') !!}		
+		{!! Html::script('assets/js/libs/bootstrap-tagsinput/bootstrap-tagsinput.min.js') !!}
+		{!! Html::script('assets/js/libs/multi-select/jquery.multi-select.js') !!}
+		{!! Html::script('assets/js/libs/inputmask/jquery.inputmask.bundle.min.js') !!}
+		{!! Html::script('assets/js/libs/moment/moment.min.js') !!}
+		{!! Html::script('assets/js/libs/bootstrap-datepicker/bootstrap-datepicker.jss') !!}
+		{!! Html::script('assets/js/libs/bootstrap-colorpicker/bootstrap-colorpicker.min.js') !!}
+		{!! Html::script('assets/js/core/demo/DemoFormComponents.js') !!}
+		{!! Html::script('assets/js/core/demo/DemoTableDynamic.js') !!}
 		
 		<script type="text/javascript">	
-		   																 //Include the jquery table functionality for the table like search, page numbers, no of rows to be displayed
-																		//and previous and next button to toggle between pages.
+		    //Include the jquery table functionality for the table like search, page numbers, no of rows to be displayed
+			//and previous and next button to toggle between pages.
 			$(document).ready(function() {
 			    $('#datatable1').DataTable();
 			    "aoColumnDefs": [
@@ -56,10 +56,10 @@
 			       ]
 			} );
 
-																		//Remove the sort column icon from the last two columns on the table
-																		//$('#datatable1').dataTable( {
+			//Remove the sort column icon from the last two columns on the table
+		//	$('#datatable1').dataTable( {
 			      
-																		//	});
+		//	});
 		</script>
 
-																		<!-- END JAVASCRIPT -->
+		<!-- END JAVASCRIPT -->
