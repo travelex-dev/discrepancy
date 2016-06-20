24f4948f44c654f78ef9e3ce3f939cb17a6ce785
AnijGurung <aniz_grg@hotmail.com>
2016-06-20 15:15:33 +0100

    The link is changed to proper HTML link
    All the links have been updated to HTML::script format.

--- discrepancy-dev/resources/views/includes/head.blade.php
+++ discrepancy-dev/resources/views/includes/head.blade.php
@@ -8,31 +8,29 @@
 		<!-- END META -->
 
 		<!-- BEGIN STYLESHEETS -->
-		<link href='http://fonts.googleapis.com/css?family=Roboto:300italic,400italic,300,400,500,700,900' rel='stylesheet' type='text/css'/>
 		
-		<link type="text/css" rel="stylesheet" href="{{ URL::asset('assets/css/theme-default/bootstrap.css') }}" />
-		<link type="text/css" rel="stylesheet" href="{{ URL::asset('assets/css/theme-default/materialadmin.css') }}" />
-		<link type="text/css" rel="stylesheet" href="{{ URL::asset('assets/css/theme-default/font-awesome.min.css') }}" />
-		<link type="text/css" rel="stylesheet" href="{{ URL::asset('assets/css/theme-default/material-design-iconic-font.min.css') }}" />
+		{!! HTML::style('http://fonts.googleapis.com/css?family=Playfair+Display') !!}
+		{!! HTML::style('assets/css/theme-default/bootstrap.css') !!}
+		{!! HTML::style('assets/css/theme-default/materialadmin.css') !!}
+		{!! HTML::style('assets/css/theme-default/font-awesome.min.css') !!}
+		{!! HTML::style('assets/css/theme-default/material-design-iconic-font.min.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/rickshaw/rickshaw.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/morris/morris.core.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/wizard/wizard.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/DataTables/jquery.dataTables.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/DataTables/extensions/dataTables.colVis.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/DataTables/extensions/dataTables.tableTools.css') !!}
 
-		<link type="text/css" rel="stylesheet" href="{{ URL::asset('assets/css/theme-default/libs/rickshaw/rickshaw.css') }}" />
-		<link type="text/css" rel="stylesheet" href="{{ URL::asset('assets/css/theme-default/libs/morris/morris.core.css') }}" />
-		<link type="text/css" rel="stylesheet" href="{{ URL::asset('assets/css/theme-default/libs/wizard/wizard.css') }}" />
-		<link type="text/css" rel="stylesheet" href="{{ URL::asset('assets/css/theme-default/libs/DataTables/jquery.dataTables.css') }}" />
-		<link type="text/css" rel="stylesheet" href="{{ URL::asset('assets/css/theme-default/libs/DataTables/extensions/dataTables.colVis.css') }}" />
-		<link type="text/css" rel="stylesheet" href="{{ URL::asset('assets/css/theme-default/libs/DataTables/extensions/dataTables.tableTools.css') }}" />
-		<style type="text/css"></style><style type="text/css"></style>
-		
-
-		<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/libs/select2/select2.css?1424887856" />
-		<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/libs/multi-select/multi-select.css?1424887857" />
-		<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/libs/bootstrap-datepicker/datepicker3.css?1424887858" />
-		<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/libs/jquery-ui/jquery-ui-theme.css?1423393666" />
-		<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/libs/bootstrap-colorpicker/bootstrap-colorpicker.css?1424887860" />
-		<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/libs/bootstrap-tagsinput/bootstrap-tagsinput.css?1424887862" />
-		<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/libs/typeahead/typeahead.css?1424887863" />
-		<link type="text/css" rel="stylesheet" href="../../assets/css/theme-default/libs/dropzone/dropzone-theme.css?1424887864" />
-		<!-- END STYLESHEETS -->
+		{!! HTML::style('assets/css/theme-default/libs/select2/select2.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/multi-select/multi-select.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/bootstrap-datepicker/datepicker3.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/jquery-ui/jquery-ui-theme.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/bootstrap-colorpicker/bootstrap-colorpicker.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/bootstrap-tagsinput/bootstrap-tagsinput.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/typeahead/typeahead.css') !!}
+		{!! HTML::style('assets/css/theme-default/libs/dropzone/dropzone-theme.css') !!}		
+				
+			<!-- END STYLESHEETS -->
 
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
